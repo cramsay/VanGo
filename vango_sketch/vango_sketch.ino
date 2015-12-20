3af163455677b70a7d6145512f0b92440314d98d
greglaw95 <greg.law95@googlemail.com>
2015-12-20 17:28:50 +0000

    A basic plan of how I aim to control the motor speeds.

--- vango_sketch/vango_sketch.ino
+++ vango_sketch/vango_sketch.ino
@@ -1,31 +1,58 @@
-/* Main loop
- *  
- *  As it stands, this is a wee demo of how to
- *  interface with the wheel tracking module.
- *  
- *  Prints wheel positions to serial every second.
- *  Hardware setup:
- *    2 x IR sensor
- *      -> Sensor D0's to pins 49 (x axis) and 48 (y axis)
+/*
+ * At the moment this is how I see the main body of the 
+ * code working. The areas enclosed in comments using /*
+ * are what I intend to fill in myself and those using
+ * // are currently there to leave in lines of code I'll
+ * need to call from other peoples sections.
  */
-
 #include "tracking.h"
 
-WheelPos pos = {0,0,0,0};
+#define accuracyLimit 2
+
+WheelPos target ={0,0,0,0};
+WheelPos current ={0,0,0,0};
+WheelPos diff={0,0,0,0};
 
 void setup() {
-  Serial.begin(115200);
-  
   trackInit();
-  trackSetDir(1,1);
+  //adamStuff();
 }
 
 void loop() {
-  trackGetPos(&pos);
-  
-  Serial.print(pos.x1);
-  Serial.print(",");
-  Serial.println(pos.y1);
-  
-  delay(1000);
+  //nextCoord(&target);
+  trackGetPos(&current);
+  diff=getDiff(target,current);
+  int xdiff=(diff.x1+diff.x2)/2;
+  int ydiff=(diff.y1+diff.y2)/2;
+  while((xdiff>accuracyLimit)&&(ydiff>accuracyLimit)){
+    /*
+     * find ratio between x and y
+     * find smallest applicable approximation of ratio using 
+     * whole numbers basically
+     * adjust to balance x and y while maintaining ratio so if 
+     * x1 and x2 aren't the same this can be fixed
+     * apply these values to motors
+     * short loop
+     * this loop is used to allow it to drive forward for a 
+     * while adjusting the motor speeds at set intervals
+     * to try to get them to what was intended accounting
+     * for different environmental issues like them being better
+     * in one direction than the other, etc
+     *      wait very small amount of time
+     *      check velocities
+     *      adjust motors
+     * check position
+     * find average x difference
+     * find average y difference
+     */
+  }
 }
+
+WheelPos getDiff(WheelPos target,WheelPos current){
+  /*
+   * Do stuff mainly subtraction I'd guess really but I'm
+   * working out high level stuff just now.
+   */
+  return target;
+}
+
