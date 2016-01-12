852f71ad80d2879bf9f3830b47e3427f95a3861d
Craig Ramsay <cramsay01@gmail.com>
2016-01-12 16:53:52 +0000

    Fixed pen mech. Adafruit shield init was clobbering settings

--- vango_sketch/vango_sketch.ino
+++ vango_sketch/vango_sketch.ino
@@ -4,31 +4,34 @@
 #include <Servo.h>
 
 //The pin on the arduino that the servo has been connected to.
-#define servoPin 8
+#define SERVO_PIN 9
 //The servo setting to lift the pen.
-#define penUp 180
+#define PEN_UP 180
 //The servo setting to drop the pen
-#define penDown 130
+#define PEN_DOWN 130
 
-Servo penLift;
+Servo pen;
 int penLocation;
 WheelPos target;
 
 void setup() {
-  penLift.attach(servoPin);
-  
   trackInit();
   comsInit();
   ctrlInit();
+  
+  pen.attach(SERVO_PIN);
 }
 
 void loop() {
   comsGetNextInstr(&target,&penLocation);
-  
-  if(penLocation==1)
-    penLift.write(penUp);
-  else
-    penLift.write(penDown);
+
+  if(penLocation==1){
+    pen.write(PEN_UP);
+    Serial1.println("Pen going up");
+  } else {
+    pen.write(PEN_DOWN);
+    Serial1.println("Pen going down");
+  }
 
   ctrlToNextCoord(&target);
 }
